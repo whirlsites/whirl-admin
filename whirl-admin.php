574b429ce3a803be98d345d7fe78429b79b8b030
whirlsites <tommy@Tommys-MacBook-Pro.local>
2014-06-04 16:57:07 -0400

    Main Plugin File 1.0
    Added plugin file

---
+++ whirl-admin.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Plugin Name: Whirl Sites Admin Plugin
+ * Plugin URI: http://www.whirlsites.com
+ * Description: This plugin is required for all Whirl Sites clients and adds special site functionality.
+ * Author: Whirl Sites
+ * Author URI: http://www.whirlsites.com
+ * Version: 0.1.0
+ */
+ 
+// GitHub Updates
+ 
+include_once('updater.php');
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/whirlsites/whirl-admin', // the github API url of your github repo
+        'raw_url' => 'https://raw.github.com/whirlsites/whirl-admin/master', // the github raw url of your github repo
+        'github_url' => 'https://github.com/whirlsites/whirl-admin', // the github url of your github repo
+        'zip_url' => 'https://github.com/whirlsites/whirl-admin/zipball/master', // the zip url of the github repo
+        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.0', // which version of WordPress does your plugin require?
+        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+        'readme' => 'readme.txt' // which file to use as the readme for the version number
+    );
+    new WPGitHubUpdater($config);
+}
+
+// Child Page Conditional
+
+function is_subpage() {
+    global $post;                              // load details about this page
+
+    if ( is_page() && $post->post_parent ) {   // test to see if the page has a parent
+        return $post->post_parent;             // return the ID of the parent post
+
+    } else {                                   // there is no parent so ...
+        return false;                          // ... the answer to the question is false
+    }
+}
+
+// Remove WordPress Version
+
+remove_action( 'wp_head', 'wp_generator' );
+
+// WordPress Menus
+
+add_action( 'init', 'register_my_menu' );
+
+function register_my_menu() {
+	register_nav_menu( 'main-menu', __( 'Main Menu' ) );
+	register_nav_menu( 'footer-menu', __( 'Footer Menu' ) );
+}
+
+// Add Dashboard Widget
+
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+
+function my_custom_dashboard_widgets() {
+global $wp_meta_boxes;
+
+wp_add_dashboard_widget('custom_help_widget', 'Whirl Sites', 'custom_dashboard_help');
+}
+
+function custom_dashboard_help() {
+echo '<p>Welcome to your Whirl Site! Need help? Contact us at <a href="mailto:help@whirlsites.com">here</a>.</p>';
+}
+
+// Theme Customizer
+
+function whirl_custom( $wp_customize ) {
+  //All our sections, settings, and controls will be added here
+}
+add_action( 'customize_register', 'whirl_custom' );
+
+?>

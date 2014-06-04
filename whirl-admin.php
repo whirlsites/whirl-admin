85fee63343ac2c534ac9733687748c26e9520d2d
whirlsites <tommy@Tommys-MacBook-Pro.local>
2014-06-04 17:09:20 -0400

    Test

--- whirl-admin.php
+++ whirl-admin.php
@@ -5,27 +5,29 @@
  * Description: This plugin is required for all Whirl Sites clients and adds special site functionality.
  * Author: Whirl Sites
  * Author URI: http://www.whirlsites.com
- * Version: 0.1.0
+ * Version: 1.1
  */
  
 // GitHub Updates
+
+// Update Test
  
 include_once('updater.php');
 
 if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
     $config = array(
         'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
+        'proper_folder_name' => 'whirl-admin', // this is the name of the folder your plugin lives in
         'api_url' => 'https://api.github.com/repos/whirlsites/whirl-admin', // the github API url of your github repo
         'raw_url' => 'https://raw.github.com/whirlsites/whirl-admin/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/whirlsites/whirl-admin', // the github url of your github repo
         'zip_url' => 'https://github.com/whirlsites/whirl-admin/zipball/master', // the zip url of the github repo
-        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'sslverify' => true, // 
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
         'readme' => 'readme.txt' // which file to use as the readme for the version number
     );
-    new WPGitHubUpdater($config);
+    new WP_GitHub_Updater($config);
 }
 
 // Child Page Conditional
